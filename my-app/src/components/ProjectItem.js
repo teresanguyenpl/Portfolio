f770b61334b2c6f499c463b54306701b99088526
Teresa Nguyen <teresanguyenpl@gmail.com>
2022-01-13 17:32:01 -0800

    done project page

--- my-app/src/components/ProjectItem.js
+++ my-app/src/components/ProjectItem.js
@@ -3,31 +3,44 @@
 import Cleverchat from '../assets/Images/CC.jpg'
 import TheForm from '../assets/Images/Forms.jpg'
 import Shopfies from '../assets/Images/Shop.jpg'
+import Portfolio from '../assets/Images/Portfolio.jpg'
 
 export const ProjectItems = [
     {
         id: 1,
         name: "CleverChat",
-        used: ["NodeJS", "JavaScript", "Heroku", "Postman", "Android Studio", "Java", "PostgreSQL"],
+        used: "NodeJS, JavaScript, Heroku, Postman, Android Studio, Java, PostgreSQL",
         date: "Nov 2021 - Dec 2021",
         img: Cleverchat,
-        link: "https://github.com/teresanguyenpl/CleverChat-Front",
-        link2: "https://github.com/teresanguyenpl/CleverChat-Back"
+        link: "Cleverchat"
+        // link: "https://github.com/teresanguyenpl/CleverChat-Front",
+        // link2: "https://github.com/teresanguyenpl/CleverChat-Back"
     },
     {
         id: 2,
-        name: "Form",
-        used: ["Html", "Css", "JavaScript"],
+        name: "Login - Register Forms",
+        used: "Html, Css, JavaScript",
         date: "Dec 2021 - Jan 2022",
         img: TheForm,
-        link: "https://github.com/teresanguyenpl/CleverChat-Front"
+        link: "Form"
+        // link: "https://github.com/teresanguyenpl/CleverChat-Front"
     },
     {
         id: 3,
         name: "Shopfies",
-        used: ["PHP", "Html", "Css", "GCloud(GCP)", "Bootstrap"],
+        used: "PHP, Html, Css, GCloud(GCP), Bootstrap",
         date: "Nov 2021 - Dec 2021",
         img: Shopfies,
-        link: "https://github.com/teresanguyenpl/Shopfies-1"
+        link: "Shopfies"
+        // link: "https://github.com/teresanguyenpl/Shopfies-1"
+    },
+    {
+        id: 4,
+        name: "Portfolio",
+        used: "ReactJS, Styled-component",
+        date: "January 2022",
+        img: Portfolio,
+        link: ""
+        // link: "https://github.com/teresanguyenpl/Portfolio"
     }
 ]
--- my-app/src/components/ProjectStyle.js
+++ my-app/src/components/ProjectStyle.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
 import styled from 'styled-components'
+import { gold, blue, gray } from "./Styles";
 
 const Contain = styled(NavLink)`
 width: calc(10rem + 15vw);
@@ -19,18 +20,27 @@ box-shadow: 0 0 1rem 0 black;
 const Image = styled.div`
 background-image: ${props => `url(${props.img})`};
 width: 100%;
-height: 60%;
+height: 55%;
 background-size: cover;
 background-position: center center;
 `
 
 const Title = styled.h3 `
 color: inherit;
-padding-top: 0.5rem;
+padding-top: 0.3rem;
 font-weight: 100;
 text-align: center;
+color: ${gold};
 `
 
+const Used = styled.h5`
+padding-top: 0.1rem;
+`
+
+
+const Date = styled.h6`
+padding-top: 0.1rem;
+`
 
 const ProjectItems = (props) => {
     const {name, used, date, img, link} = props.project;
@@ -38,6 +48,8 @@ const ProjectItems = (props) => {
         <Contain target="_blank" to={{pathname: link}}>
             <Image img={img} />
             <Title>{name}</Title>
+            <Used>{used}</Used>
+            <Date>{date}</Date>
         </Contain>
     )
 }
